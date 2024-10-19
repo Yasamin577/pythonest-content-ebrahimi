d36a85685350107ffa177c21196f472e3f67e334
Talabuddin Gheyasi <talabuddin.gheyasi@gmail.com>
2024-10-19 19:21:28 +0430

    Chapter 7

---
+++ 1.pcep/7. Conditions/test.py
@@ -0,0 +1,28 @@
+if condition1:
+    # Block of code if condition1 is true
+    if condition2:
+        # Block of code if condition2 is true
+    else:
+        # Block of code if condition2 is false
+else:
+    # Block of code if condition1 is false
+
+
+
+# age = 18
+
+# if age >= 18:
+#     print("You are an .")
+
+
+# if score >= 90:
+#     print("Grade: A")
+# elif score >= 80:
+#     print("Grade: B")
+# elif score >= 70:
+#     print("Grade: C")
+# elif score >= 60:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")
+ 
